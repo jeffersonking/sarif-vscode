18c96f576d021204b9d960a97f2c23f6c5b4e24a
Jeff King <jeffking@gmail.com>
2020-04-24 16:57:36 -0700

    Graceful Log upgrade fail.

--- src/context/loadLogs.ts
+++ src/context/loadLogs.ts
@@ -1,4 +1,4 @@
-import { spawnSync } from 'child_process'
+import { execSync } from 'child_process'
 import * as fs from 'fs'
 import { join } from 'path'
 import { Log } from 'sarif'
@@ -9,12 +9,20 @@ import { Store } from '.'
 import { augmentLog } from '../shared'
 
 export async function loadLogs(uris: Uri[]) {
-	const logs = uris.map(uri => {
-		const file = fs.readFileSync(uri.fsPath, 'utf8') // Assume scheme file.
-		const log = JSON.parse(file) as Log
-		log._uri = uri.toString()
-		return log
-	})
+	const logs = uris.slice(0, 20)
+		.map(uri => {
+			try {
+				const file = fs.readFileSync(uri.fsPath, 'utf8')  // Assume scheme file.
+					.replace(/^\uFEFF/, '') // Trim BOM.
+				const log = JSON.parse(file) as Log
+				log._uri = uri.toString()
+				return log
+			} catch (error) {
+				window.showErrorMessage(`Failed to parse '${uri.fsPath}'`)
+				return undefined
+			}
+		})
+		.filter(log => log)
 	const logsNoUpgrade = [] as Log[]
 	const logsToUpgrade = [] as Log[]
 	let warnUpgradeExtension = logs.some(log => detectUpgrade(log, logsNoUpgrade, logsToUpgrade))
@@ -26,15 +34,20 @@ export async function loadLogs(uris: Uri[]) {
 				for (const [i, oldLog] of logsToUpgrade.entries()) {
 					progress.report({
 						message: `Upgrading ${i + 1} of ${upgrades} log${upgrades === 1 ? '' : 's'}...`,
-						increment: i / upgrades * 100
+						increment: 1 / upgrades * 100
 					})
 					await new Promise(r => setTimeout(r, 0)) // Await needed for progress to update
-					const tempPath = upgradeLog(Uri.parse(oldLog._uri).fsPath)
-					const file = fs.readFileSync(tempPath, 'utf8') // Assume scheme file.
-					const log = JSON.parse(file) as Log
-					log._uri = oldLog._uri
-					log._uriUpgraded = Uri.file(tempPath).toString()
-					logsNoUpgrade.push(log)
+					const {fsPath} = Uri.parse(oldLog._uri)
+					try {
+						const tempPath = upgradeLog(fsPath)
+						const file = fs.readFileSync(tempPath, 'utf8') // Assume scheme file.
+						const log = JSON.parse(file) as Log
+						log._uri = oldLog._uri
+						log._uriUpgraded = Uri.file(tempPath).toString()
+						logsNoUpgrade.push(log)
+					} catch {
+						window.showErrorMessage(`Failed to upgrade '${fsPath}'`)
+					}
 				}
 			}
 		)
@@ -68,18 +81,8 @@ export function detectUpgrade(log: Log, logsNoUpgrade: Log[], logsToUpgrade: Log
 
 export function upgradeLog(path: string) {
 	const name = tmpNameSync()
-	try {
-		const multitoolExe = `Sarif.Multitool${process.platform === 'win32' ? '.exe' : ''}`
-		const {error} = spawnSync(join(Store.extensionPath || process.cwd(), 'out', multitoolExe), [
-			'transform',
-			path,
-			'--force',
-			'--pretty-print',
-			'--output', name
-		], { stdio: 'inherit' })
-		if (error) console.warn('RESULTERROR', error)
-	} catch(e) {
-		console.warn('ERROR', path, e) // Use malformed sarif to test
-	}
+	const multitoolExe = `Sarif.Multitool${process.platform === 'win32' ? '.exe' : ''}`
+	const multitoolExePath = join(Store.extensionPath || process.cwd(), 'out', multitoolExe)
+	execSync(`${multitoolExePath} transform ${path} --force --pretty-print --output ${name}`)
 	return name
 }
